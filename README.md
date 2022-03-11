cdbbc5bbcee01a082a2481cf0f30070d224f7476
aouos <aouos@qq.com>
2022-03-11 11:10:26 +0800

    feat: init template and script

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # React-Vite
 
 create react app with vite
+
+## install
+
+```bash
+npm install -g react-vite
+```
+
+## create react vite project
+
+```bash
+react-vite
+```
--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 #!/usr/bin/env node
-
+const { program } = require('commander');
+const path = require('path');
 const inquirer = require('inquirer');
+const fs = require('fs-extra');
+const chalk = require('chalk');
+
+program
+  .version(require('./package.json').version)
+  .option('-v, --version', 'react-vite version');
+
+program.parse(process.argv);
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'project-name',
+      name: 'name',
       message: 'Input your project name',
       default: 'react-vite',
     },
+    {
+      type: 'confirm',
+      name: 'typescript',
+      message: 'Use typescript template',
+      default: false,
+    },
+    {
+      type: 'list',
+      name: 'template',
+      choices: ['react', 'react-ts', 'react-antd'],
+      message: 'Choice the template',
+      default: 'react',
+    },
   ])
   .then((answers) => {
-    console.log(answers);
+    const { name, typescript, template } = answers;
+    const destUrl = path.join(__dirname, 'templates', template);
+
+    const cwdUrl = process.cwd();
+
+    try {
+      fs.copySync(destUrl, cwdUrl);
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(chalk.green('success'));
   })
   .catch((error) => {
     console.log(error);
