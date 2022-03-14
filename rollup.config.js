bd873c3e4494add8da3f4ee7efbef15585f55171
aouos <aouos@qq.com>
2022-03-14 11:58:29 +0800

    chore: use babel and uglify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,19 @@
-module.exports = {
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
+export default {
   input: './src/index.js',
   output: {
     file: './lib/index.js',
     format: 'cjs',
     banner: '#!/usr/bin/env node',
   },
+  plugins: [
+    resolve(),
+    uglify(),
+    babel({
+      exclude: 'node_modules',
+    }),
+  ],
 };
